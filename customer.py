3422f25345e2ece293bc69fe824d5312ae61079f
Jamal U Din <jamaluddin14926@gmail.com>
2024-09-16 16:44:10 +0500

    Customer Added

---
+++ customer.py
@@ -0,0 +1,47 @@
+# customer.py
+import json
+
+class Customer:
+    def __init__(self, account_number, name, balance, contact_info):
+        self.account_number = account_number
+        self.name = name
+        self.balance = balance
+        self.contact_info = contact_info
+
+    def to_dict(self):
+        return {
+            "account_number": self.account_number,
+            "name": self.name,
+            "balance": self.balance,
+            "contact_info": self.contact_info
+        }
+
+class CustomerManager:
+    def __init__(self):
+        self.customers = self.load_customers()
+
+    def load_customers(self):
+        try:
+            with open('customers.json', 'r') as file:
+                return json.load(file)
+        except FileNotFoundError:
+            return {}
+
+    def save_customers(self):
+        with open('customers.json', 'w') as file:
+            json.dump(self.customers, file)
+
+    def add_customer(self, account_number, name, balance, contact_info):
+        if account_number in self.customers:
+            raise ValueError("Account number already exists!")
+        self.customers[account_number] = Customer(account_number, name, balance, contact_info).to_dict()
+        self.save_customers()
+
+    def delete_customer(self, account_number):
+        if account_number not in self.customers:
+            raise ValueError("Customer does not exist!")
+        del self.customers[account_number]
+        self.save_customers()
+
+    def get_customer(self, account_number):
+        return self.customers.get(account_number, None)
